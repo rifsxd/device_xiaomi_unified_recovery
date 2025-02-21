07aaaf1006cb2eb4642da9906d0a47e995b2da00
rifsxd <rifat.44.azad.rifs@gmail.com>
2025-02-22 05:27:54 +0600

    device: increase display framerate from 60 to 120

--- device.mk
+++ device.mk
@@ -107,7 +107,7 @@ TW_INCLUDE_FASTBOOTD    := true
 TWRP_INCLUDE_LOGCAT     := true
 TW_INCLUDE_PYTHON       := true
 TW_NO_SCREEN_BLANK      := true
-TW_FRAMERATE            := 60
+TW_FRAMERATE            := 120
 
 # TWRP - Partition tools
 # PBRP source does not offer TW_ENABLE_ALL_PARTITION_TOOLS at the moment
